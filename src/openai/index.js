4344579350601b8f55955efb564c6ee102b17b67
noor-khan <foruconnect@gmail.com>
2023-02-11 19:26:52 +0500

    update key

--- src/openai/index.js
+++ src/openai/index.js
@@ -1,6 +1,6 @@
 import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
   organization: "org-ZG6Lfc20MGBvuSRJSAaKS9Vz",
-  apiKey: "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",
+  apiKey: "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",
 });
 export const openai = new OpenAIApi(configuration);
