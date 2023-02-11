e09cec20571ee01f1342f55ca9aeecbdadf70fb5
noor-khan <foruconnect@gmail.com>
2023-02-11 19:13:45 +0500

    remove env

--- src/openai/index.js
+++ src/openai/index.js
@@ -1,7 +1,6 @@
 import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
-    organization: "org-ZG6Lfc20MGBvuSRJSAaKS9Vz",
-    apiKey: "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",
+  organization: "org-ZG6Lfc20MGBvuSRJSAaKS9Vz",
+  apiKey: "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",
 });
 export const openai = new OpenAIApi(configuration);
-
